b7743f595b865742106ce49afaa06b4d75084c16
frankcamarena <fgustavocamarena@gmail.com>
2025-11-01 17:14:12 -0400

    fix: set base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // AÑADE O MODIFICA ESTA LÍNEA
+  // La base debe ser el nombre de tu repositorio, rodeado de barras diagonales.
+  base: '/tc-site/', 
 })
